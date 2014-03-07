8b53d1560ddf671215761a62c8285868f985189c
Rumen Paletov <rumen.paletov@gmail.com>
2014-03-07 15:09:44 +0200

    Use currect appbuilder path when running on OS X

--- command_executor.py
+++ command_executor.py
@@ -1,16 +1,21 @@
 import sublime
+import os
 
 from thread_progress import ThreadProgress
 from command_thread import CommandThread
 from notifier import Notifier
 
 class AppBuilderCommandExecutor(object):
-    APP_BUILDER = "appbuilder"
+    _appbuilder_path = []
+
+    DEFAULT_OSX_NODE_PATH = "/usr/local/bin/node"
+    DEFAULT_OSX_APPBUILDER_PATH = "/usr/local/bin/appbuilder"
+    DEFAULT_WIN_APPBUILDER_PATH = "appbuilder"
 
     @staticmethod
     def run_command(command, on_data=None, on_done=None, in_progress_message="Loading",
         show_status=True, filter_empty_args=True, no_save=False, **kwargs):
-        command.insert(0, AppBuilderCommandExecutor.APP_BUILDER)
+        command = AppBuilderCommandExecutor._get_app_builder_path() + command
 
         if filter_empty_args:
             command = [arg for arg in command if arg]
@@ -31,3 +36,22 @@ class AppBuilderCommandExecutor(object):
     def show_quick_panel(sublime_command, items, on_done):
         window = sublime_command.get_window()
         window.show_quick_panel(items, on_done)
+
+    @staticmethod
+    def _get_app_builder_path():
+        if not AppBuilderCommandExecutor._appbuilder_path:
+            if os.name == "nt":
+                AppBuilderCommandExecutor._appbuilder_path.append(AppBuilderCommandExecutor.DEFAULT_WIN_APPBUILDER_PATH)
+            elif os.name == "posix":
+                config = sublime.load_settings("AppBuilder.sublime-settings")
+                if config.get("node_osx_path"):
+                    AppBuilderCommandExecutor._appbuilder_path.append(config.get("node_osx_path"))
+                else:
+                    AppBuilderCommandExecutor._appbuilder_path.append(DEFAULT_OSX_NODE_PATH)
+
+                if config.get("appbuilder_osx_path"):
+                    AppBuilderCommandExecutor._appbuilder_path.append(config.get("appbuilder_osx_path"))
+                else:
+                    AppBuilderCommandExecutor._appbuilder_path.append(AppBuilderCommandExecutor.DEFAULT_OSX_APPBUILDER_PATH)
+
+        return AppBuilderCommandExecutor._appbuilder_path
