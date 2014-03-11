e273fcdb063f2cfddf53f6c5fc7e60d227c6546f
Rumen Paletov <rumen.paletov@gmail.com>
2014-03-11 14:32:03 +0200

    Introduce build script

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        copyPackageTo: "\\\\telerik.com\\Resources\\BlackDragon\\Builds\\appbuilder-sublime-package",
+        jobName: process.env["JOB_NAME"] || "local",
+        buildNumber: process.env["BUILD_NUMBER"] || "non-ci",
+        subFolder: "<%= jobName %>\\<%= grunt.template.today('yyyy-mm-dd') %> #<%= buildNumber %>",
+
+        compress: {
+            main: {
+                options: {
+                    archive: "<%= copyPackageTo %>\\<%= subFolder %>\\latest.zip"
+                },
+                files: [
+                    { src: ["**/*.{py,pyd,so}", "*.{sublime-keymap,sublime-menu,sublime-settings}", "LICENSE"] }
+                ]
+            }
+        },
+
+        clean: {
+            src: ["**/*.pyc"]
+        }
+    });
+
+    grunt.loadNpmTasks("grunt-contrib-compress");
+    grunt.loadNpmTasks("grunt-contrib-clean");
+    grunt.registerTask("default", "compress:main");
+}
