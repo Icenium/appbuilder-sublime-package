eaea00023332393d8aa6ae82fe030656c68929b4
Rumen Paletov <rumen.paletov@gmail.com>
2014-03-11 18:17:50 +0200

    Refactoring

---
+++ AppBuilder.py
@@ -0,0 +1,1 @@
+from app_builder import *
---
+++ app_builder/__init__.py
@@ -0,0 +1,11 @@
+from app_builder.devices_commands import DeployCommand
+from app_builder.devices_commands import SyncCommand
+from app_builder.devices_commands import ToggleLiveSyncCommand
+from app_builder.devices_commands import RunInSimulatorCommand
+
+__all__ = [
+    "DeployCommand",
+    "SyncCommand",
+    "ToggleLiveSyncCommand",
+    "RunInSimulatorCommand",
+]
