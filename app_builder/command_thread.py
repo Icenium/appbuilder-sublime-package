a0269e63b886f204ecf3b50a258f9fa2e2c4af59
Rumen Paletov <rumen.paletov@gmail.com>
2014-05-19 12:14:35 +0300

    Initialize the proc field of the CommandThread class

--- app_builder/command_thread.py
+++ app_builder/command_thread.py
@@ -24,6 +24,7 @@ class CommandThread(threading.Thread):
         else:
             self.stdout = subprocess.PIPE
         self.kwargs = kwargs
+        self.proc = None
 
     def terminate(self):
         if self.proc != None:
